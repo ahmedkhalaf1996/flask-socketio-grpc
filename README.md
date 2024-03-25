1cb392a864bdca6fda1c904c285009eba5402550
ahmedkhalaf7396 <ahmedkhalaf7396@gmail.com>
2024-03-25 11:25:21 +0200

    done

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # flask-socketio-grpc
+
+
+# 
+    python -m grpc_tools.protoc -I./. --python_out=. --grpc_python_out=. Notification.proto 
+
+
+    python -m grpc_tools.protoc -I./protos --python_out=protos --grpc_python_out=protos protos/Notification.proto
+
+
+
--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from concurrent import futures
-from flask import Flask, render_template
+from flask import Flask, request
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 import grpc
+import sys 
+sys.path.append('protos')
 import Notification_pb2
 import Notification_pb2_grpc
 import threading
@@ -14,40 +16,76 @@ app.config['SECRET_KEY'] = 'justasecretkeythatishouldputhere'
 
 socketio = SocketIO(app)
 CORS(app)
+user_connections = {}  # Store user connections
 
+# grpc start 
 class NotificationServicer(Notification_pb2_grpc.NotificationGrpcServiceServicer):
     def SendGrpcNotification(self, request, context):
-        print('Sending gRPC notification..')
-        query = {"hay": "here"}
-        socketio.emit('log', dict(data=str(query)))
+        if request.mainuid not in user_connections:
+            print(f"User connection not found for mainuid: {request.mainuid}")
+            return Empty()
+
+
+        usersocketconnid = user_connections[request.mainuid]
+        print('grpc uconnid', usersocketconnid)
+        msg = {
+            "_id": request._id,
+            "createdAt":request.createdAt,
+            "deatils": request.deatils,
+            "isreded": request.isreded,
+            "mainuid": request.mainuid,
+            "targetid": request.targetid,
+            "user": {
+                "avatar": request.user.avatar,
+                "name": request.user.name
+            }
+        }
+        socketio.emit('notification', dict(data=str(msg)), room=usersocketconnid)
         # You can add your logic here to send a notification over SocketIO
         return Empty()
 
 def run_grpc_server():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     Notification_pb2_grpc.add_NotificationGrpcServiceServicer_to_server(NotificationServicer(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port('[::]:8090')
     server.start()
     print('gRPC server running...')
     server.wait_for_termination()
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/api')
-def api():
-    query = {"hay": "here"}
-    socketio.emit('log', dict(data=str(query)))
-    return False
+# grpc end 
 
 @socketio.on('connect')
 def on_connect():
-    payload = dict(data='Connected')
-    emit('log', payload)
+    print("user Connected")
+
+
+@socketio.on('setUserId')
+def handle_set_user_id(user_id):
+    if user_id:
+        try:
+            user_connections[user_id] = request.sid
+            print(f'User {user_id} connected with socket id {request.sid}')
+
+            return 'User ID set successfully'
+
+        except Exception as e:
+            print(f"Error setting user ID: {e}")
+
+    return 'Failed to set user ID'
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    user_id = next((uid for uid, sid in user_connections.items() if sid == request.sid), None)
+    if user_id:
+        print(f"User disconnected: {user_id}")  # Print user ID before deletion
+        if user_id in user_connections:
+            del user_connections[user_id]  # Remove user ID from dictionary
+    else:
+        print('An unidentified user disconnected')
+
 
 if __name__ == '__main__':
     grpc_thread = threading.Thread(target=run_grpc_server)
     grpc_thread.start()
     
-    socketio.run(app)
+    socketio.run(app,  port=8088)
